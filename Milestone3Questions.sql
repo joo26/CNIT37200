eca828872c71c6496a95e61297848ecb5e4a50cc
StewMuu <151254084+StewMuu@users.noreply.github.com>
2023-11-17 23:49:51 -0500

    Update Milestone3Questions.sql

--- Milestone3Questions.sql
+++ Milestone3Questions.sql
@@ -1,17 +1,17 @@
-/* videos with most comments - Stewart */
+/* videos with most comments - (Question 1) Stewart */
 select videoinfo.videoid, sum(commentinfo.comments) as comment_total
 from videoinfo join commentinfo on videoinfo.videoid = commentinfo.videoid
 group by videoinfo.videoid
 order by comment_total desc;
 
 
-/* correlation between video popularity and upload date - Stewart*/
+/* correlation between video popularity and upload date - (Question 6) Stewart*/
 select to_char(uploaddate, 'DD-MON-YYYY') as date_uploaded, avg(viewcount) as average_views
 from videoinfo
 group by to_char(uploaddate, 'DD-MON-YYYY')
 order by to_char(uploaddate, 'DD-MON-YYYY');
 
-/* most popular upload date - Stewart */ 
+/* most popular upload date - (Question 7) Stewart */ 
 
 select to_char(uploaddate, 'DD-MON-YYYY') as date_uploaded, count(*) upload_count
 from videoinfo
@@ -21,8 +21,7 @@ fetch first 1 row only;
 
 
 
-/* Question #10 : QUERY that outputs the total_viewcount of a specified video. 
-It outputs the title of the video, and the number of views recorded. - Stewart */
+/* Query that outputs the total_viewcount of a specified video. It outputs the title of the video, and the number of views recorded. - (Question 10) Stewart */
 
 SET SERVEROUTPUT ON;
 
@@ -44,7 +43,7 @@ END;
 
 
 
-/* Output the top 3 most watched videos, select the videoID, the title, and the viewcount. Do the same for the 3 most least watched videos. - Stewart*/
+/* Output the top 3 most watched videos, select the videoID, the title, and the viewcount. Do the same for the 3 most least watched videos. - (Question 4) Stewart*/
 
 select videoid, title, viewcount 
 from videoinfo
